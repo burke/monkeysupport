88c12ab17fe1f621b188b4a67fa21500daafd756
Burke Libbey <burke@burkelibbey.org>
2009-09-04 00:48:31 -0500

    Simpler memoize. Thanks stefanpenner

--- lib/monkeysupport/inflector.rb
+++ lib/monkeysupport/inflector.rb
@@ -1,37 +1,47 @@
 module ActiveSupport
   module Inflector
-    # extend ::ActiveSupport::Memoizable
 
-    # memoize :pluralize, :singularize, :humanize
+    @_pluralize = {}
+    alias_method :__pluralize, :pluralize
+    def pluralize(word)
+      @_pluralize[word] ||= __pluralize(word)
+    end
+
+    @_singularize = {}
+    alias_method :__singularize, :singularize
+    def singularize(word)
+      @_singularize[word] ||= __singularize(word)
+    end
+
+    @_humanize = {}
+    alias_method :__humanize, :humanize
+    def humanize(word)
+      @_humanize[word] ||= __humanize(word)
+    end
     
-    alias_method :ruby_camelize, :camelize
-
+    alias_method :__camelize, :camelize
     def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
       ActiveSupport::ASC.inflector_camelize(lower_case_and_underscored_word.to_s, first_letter_in_uppercase)
     end
 
-    alias_method :_ruby_underscore, :underscore
-
+    alias_method :__underscore, :underscore
     def underscore(camel_cased_word)
       ActiveSupport::ASC.inflector_underscore(camel_cased_word)
     end
     
 
-    alias_method :_ruby_dasherize, :dasherize
-
+    alias_method :__dasherize, :dasherize
     def dasherize(underscored_word)
       ActiveSupport::ASC.inflector_dasherize(underscored_word)
     end
     
-    alias_method :_ruby_demodulize, :demodulize
-
+    alias_method :__demodulize, :demodulize
     def demodulize(class_name_in_module)
       ActiveSupport::ASC.inflector_demodulize(class_name_in_module)
     end
 
 
-    alias_method :_ruby_parameterize, :parameterize
-    
+    alias_method :__parameterize, :parameterize
     def parameterize(string, sep = '-')
       parameterized_string = transliterate(string)
 
@@ -39,15 +49,13 @@ module ActiveSupport
     end
     
 
-    alias_method :_ruby_foreign_key, :foreign_key
-    
+    alias_method :__foreign_key, :foreign_key
     def foreign_key(class_name, separate_class_name_and_id_with_underscore = true)
       ActiveSupport::ASC.inflector_foreign_key(class_name, separate_class_name_and_id_with_underscore)
     end
 
 
-    alias_method :_ruby_ordinalize, :ordinalize
-
+    alias_method :__ordinalize, :ordinalize
     def ordinalize(number)
       x = number.to_i
       if x.class == Fixnum
