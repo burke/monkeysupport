97ca68fd9de724ceaec6739e6d8080917e1a915d
Burke Libbey <burke@burkelibbey.org>
2009-12-28 16:59:10 -0600

    readme updated to be marginally useful

--- lib/monkeysupport.rb
+++ lib/monkeysupport.rb
@@ -1,12 +1,16 @@
-require 'monkeysupport_c'
-
 require 'monkeysupport/c_proxy'
 require 'monkeysupport/memoizable'
 
-require 'monkeysupport/activesupport/inflector'
+require 'monkeysupport_c'
 
-if ["1.8.7", "1.9.1"].include? RUBY_VERSION
-  require 'output_safety_ext'
-else
-  puts "** MonkeySupport: output_safety module was not designed to work with #{RUBY_VERSION}. Extension not loaded."
+unless $MonkeyModuleExcludes && $MonkeyModuleExcludes.include? "inflector"
+  require 'monkeysupport/activesupport/inflector'
+end
+  
+unless $MonkeyModuleExcludes && $MonkeyModuleExcludes.include? "output_safety"
+  if ["1.8.7", "1.9.1"].include? RUBY_VERSION
+    require 'output_safety_ext'
+  else
+    puts "** MonkeySupport: output_safety module was not designed to work with #{RUBY_VERSION}. Extension not loaded."
+  end
 end
